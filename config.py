a04893f8d99914d9c41e32ce5992ba685a84ebf9
Ilya Rudnyanskiy <rudnianskiy@gmail.com>
2018-02-03 03:26:12 +0300

    Register user

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
